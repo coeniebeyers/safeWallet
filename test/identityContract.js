d7ea00e2a7327be0c72af7d5d511d5b456dda8b8
Jonas Tomanga <tcelleb@gmail.com>
2018-08-17 11:24:43 +0200

    Test non-existing customer

--- test/identityContract.js
+++ test/identityContract.js
@@ -3,13 +3,18 @@ let instance;
 
 contract('IdentityContract', (accounts) => {
   beforeEach(async () => {
-    instance = await IdentityContractAbstraction.new({from: accounts[0]});
+    instance = await IdentityContractAbstraction.new({ from: accounts[0] });
   });
 
   it('should be able to add a customer', async () => {
-    const txHash = await instance.addCustomer(accounts[1])
-    const result = await instance.customers(accounts[1])
-    assert.strictEqual(result.toNumber(), 1)
+    const txHash = await instance.addCustomer(accounts[1]);
+    const result = await instance.customers(accounts[1]);
+    assert.strictEqual(result.toNumber(), 1);
+  });
+
+  it('should return 0 for non-existing customer', async () => {
+    const result = await instance.customers(accounts[1]);
+    assert.strictEqual(result.toNumber(), 0);
   });
 
 });
